a66919760ef8ac99b8d3c2a23ac7a10af91b90c9
PuZZleDucK <puzzleduck@gmail.com>
2015-07-05 03:30:03 +1000

    Limit display to 100. Started styling css. Cleaned up photostory import.

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ var LatLon = function(la, lo) {
   }
 };
 
-var LocationData = function(t,d,la,lo) {
-  this.title = t,
-  this.details = d,
-  this.latlon = new LatLon(la, lo)
+var LocationData = function(t,d,la,lo,s) {
+  this.title = t;
+  this.details = d;
+  this.img = "";
+  this.src = s;
+  this.latlon = new LatLon(la, lo);
 };
 
 
@@ -70,22 +72,30 @@ window.onload = function() {
   function parse_photo_stories(data){
 //Title	URL	Date	Primary image	Primary image caption	Primary image rights information	Subjects	Station	State	Place	Keywords	Latitude	Longitude	MediaRSS URL
     var lines = data.split("\n");
-    console.log("photo_stories: ..."+data.substr(10,50)+"... ("+lines.length+"lines)");
+    console.log("photo_stories: ..."+data.substr(10,50)+"... ("+lines.length+" lines)");
     for(var line = 0; line < lines.length; line++) {
       var fields = lines[line].split(",");
-      if(fields.length === 14) {
-        console.log("photo");
+      if(line !== 0 && fields[11] !== "" && fields[12] !== "") { //&& fields.length === 14 && fields[11] !== ""
         var t = fields[0];
-        var d = fields[4];
+        var d = fields[4]+" ("+fields[9]+", "+fields[8]+": "+fields[10]+" - "+fields[6]+")" ;
         var la = fields[11];
         var lo = fields[12];
-        allData[dataCount] = new LocationData(t,d,la,lo);
-        dataCount++;
+        if(t.toLowerCase().contains("indigenous")
+            || t.toLowerCase().contains("aboriginal")
+            || t.toLowerCase().contains("australian")
+            || d.toLowerCase().contains("indigenous")
+            || d.toLowerCase().contains("aboriginal")
+            || d.toLowerCase().contains("australian")) { 
+          allData[dataCount] = new LocationData(t,d,la,lo,"ABC");
+          allData[dataCount].img = fields[3];
+          dataCount++;
+        }
       }
     }
   }
 
   function parse_heritage(data){
+//MCCID_INT	Feature_name	Feature_long_description	Feature_short_description	Location_description	Suburb	Feature_type	Event_Activity	Date_from	Date_to	Purpose	Epoch	Language_group	Clan (people)	Nation (group common name)	Source	Source_type	URL	Source_person	Aboriginal_individual	Aboriginal_individual_Gender	Non_Aboriginal_indiv	Non_Aboriginal_indiv_Gender	Non_Aboriginal_organisation    Past and present 	Non_Aboriginal_role	Melway	Aboriginal_words	Aboriginal_words_meaning	European_site_names	Physical_evidence	Address   -> adding lat long the hard way
     var lines = data.split("\n");
     console.log("heritage: ..."+data.substr(10,50)+"... ("+lines.length+"lines)");
     for(var line = 0; line < lines.length; line++) {
@@ -94,7 +104,6 @@ window.onload = function() {
       //comoplex address data.
       }
     }
-//MCCID_INT	Feature_name	Feature_long_description	Feature_short_description	Location_description	Suburb	Feature_type	Event_Activity	Date_from	Date_to	Purpose	Epoch	Language_group	Clan (people)	Nation (group common name)	Source	Source_type	URL	Source_person	Aboriginal_individual	Aboriginal_individual_Gender	Non_Aboriginal_indiv	Non_Aboriginal_indiv_Gender	Non_Aboriginal_organisation    Past and present 	Non_Aboriginal_role	Melway	Aboriginal_words	Aboriginal_words_meaning	European_site_names	Physical_evidence	Address
   }
 
   function parse_sculptures(data){
@@ -113,25 +122,35 @@ window.onload = function() {
         var d = fields[0];
         var la = fields[2].substr(2,fields[3].length);
         var lo = fields[3].substr(1,fields[3].length-3);
-        allData[dataCount] = new LocationData(t,d,la,lo);
+        allData[dataCount] = new LocationData(t,d,la,lo,"MAS");
         dataCount++;
       }
     }
   }
 
   function parse_organizations(data){
-//Organisation	Street	Suburb/Town	Postcode	Website
-    console.log("organizations: ..."+data.substr(10,50)+"...");
+//Organisation	Street	Suburb/Town	Postcode	Website    
+    var lines = data.split("\n");
+    console.log("organizations: ..."+data.substr(10,50)+"... ("+lines.length+"lines)");
+    for(var line = 0; line < lines.length; line++) {
+      var fields = lines[line].split(",");
+      if(line > 0 && fields[4]) {
+        var t = fields[0];
+        var d = fields[4]+" - "+fields[1]+", "+fields[2];
+        allData[dataCount] = new LocationData(t,d,0,0,"ORG");
+        dataCount++;
+      }
+    }
   }
 
   function parse(data){
     console.log("DATA: ..."+data.substr(10,50)+"...");
   }
 
-  load("dat/photo_stories.csv");
+//  load("dat/photo_stories.csv");
   load("dat/indigenous_heritage.csv");
-  load("dat/memorials_and_sculptures.csv");
-  load("dat/indiginous_organizations.csv");
+//  load("dat/memorials_and_sculptures.csv");
+//  load("dat/indiginous_organizations.csv");
 
 //  load("dat/atsic_regions.mid");
 //  load("dat/atsic_regions.mif");
@@ -166,8 +185,8 @@ window.onload = function() {
   function geoCallback(position)
   {
     lonlat = "["+position.coords.longitude+","+position.coords.latitude+"]";
-    //console.log("GL: "+lonlat);
-    geolocation = lonlat;
+    console.log("GL: "+lonlat);
+    //geolocation = lonlat;
     return;
   }
 
@@ -196,7 +215,7 @@ window.onload = function() {
   var delay = function() {
     if(lonlat === undefined || allData.length !== old_count) {//we want it to match
         old_count = allData.length;
-        console.log("undef"); //not ready untill user interacts
+        console.log("undef:"+allData.length); //not ready untill user interacts
         setTimeout(delay, 250);//wait 50 millisecnds then recheck
         return;
     } else {
@@ -212,11 +231,36 @@ window.onload = function() {
       output_div.innerHTML += "<h5> - location 1</h5>";
       output_div.innerHTML += "<h5> - location 2</h5>";
       
-      for(var i = 0; i < allData.length; i++) {
-        output_div.innerHTML += "<h5>"+allData[i].title+"["+allData[i].latlon.show()+"]</h5>";
+      for(var i = 0; i < allData.length && i < 100; i++) {
+//        if(allData[i].img === "") {
+//          output_div.innerHTML += "<h5>"+allData[i].title+"["+allData[i].latlon.show()+"]</h5>";
+//        } else {
+//          output_div.innerHTML += "<h5>"+allData[i].title+" ## "+allData[i].details+"["+allData[i].latlon.show()+"]</h5><img src="+allData[i].img+"></img>";
+//        }
+        
+        
+        switch(allData[i].src) {
+          case "ABC":
+            console.log("ABC");
+            output_div.innerHTML += "<div class=\"abc\"><h5>"+allData[i].title+" ## "
+                                 +allData[i].details+"["+allData[i].latlon.show()
+                                 +"]</h5><img src="+allData[i].img+"></img></div>";
+            break;
+          case "MAS":
+            console.log("MAS");
+            output_div.innerHTML += "<div class=\"mas\"><h5>"+allData[i].title+"["
+                                 +allData[i].latlon.show()+"]</h5></div>";
+            break;
+          case "ORG":
+            console.log("ORG");
+            output_div.innerHTML += "<div class=\"org\"><h5>"+allData[i].title+"["
+                                 +allData[i].latlon.show()+"]</h5></div>";
+            break;
+          default:
+            console.log("Unknown data source");
+            break;
+         }
       }
-      
-      
       return;
     }
   }
