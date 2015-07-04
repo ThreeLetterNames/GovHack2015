1f598f203d5e94991f634275407c279f1d37647b
PuZZleDucK <puzzleduck@gmail.com>
2015-07-04 18:35:35 +1000

    preparing for parsing

--- index.js
+++ index.js
@@ -10,38 +10,38 @@ window.onload = function() {
     request = new XMLHttpRequest();
     request.open('GET', file, false);
     request.send();
-    console.log("Got: "+file);
+    //console.log("Got: "+file);
     switch(file) {
       case "dat/photo_stories.csv":
         parse_photo_stories(request.responseText);
-        console.log("dat/photo_stories.csv:");
+        //console.log("dat/photo_stories.csv:");
         break;
       case "dat/indigenous_heritage.csv":
-        parse(request.responseText);
-        console.log("dat/indigenous_heritage.csv:");
+        parse_heritage(request.responseText);
+        //console.log("dat/indigenous_heritage.csv:");
         break;
       case "dat/memorials_and_sculptures.csv":
-        parse(request.responseText);
-        console.log("dat/memorials_and_sculptures.csv:");
+        parse_sculptures(request.responseText);
+        //console.log("dat/memorials_and_sculptures.csv:");
         break;
       case "dat/indiginous_organizations.csv":
-        parse(request.responseText);
-        console.log("dat/indiginous_organizations.csv:");
+        parse_organizations(request.responseText);
+        //console.log("dat/indiginous_organizations.csv:");
         break;
       case "dat/atsic_regions.mid":
-        parse(request.responseText);
+        //parse(request.responseText);
         console.log("dat/atsic_regions.mid:");
         break;
       case "dat/atsic_regions.mif":
-        parse(request.responseText);
+        //parse(request.responseText);
         console.log("dat/atsic_regions.mif:");
         break;
       case "dat/atsic_wards.mid":
-        parse(request.responseText);
+        //parse(request.responseText);
         console.log("dat/atsic_wards.mid");
         break;
       case "dat/atsic_wards.mif":
-        parse(request.responseText);
+        //parse(request.responseText);
         console.log("dat/atsic_wards.mif:");
         break;
       default:
@@ -51,25 +51,38 @@ window.onload = function() {
 
 
   function parse_photo_stories(data){
-    console.log("parse_photo_stories: ..."+data.substr(10,50)+"..."); //50 chars
+    console.log("photo_stories: ..."+data.substr(10,50)+"..."); //50 chars
+//Title	URL	Date	Primary image	Primary image caption	Primary image rights information	Subjects	Station	State	Place	Keywords	Latitude	Longitude	MediaRSS URL
+  }
+
+  function parse_heritage(data){
+    console.log("heritage: ..."+data.substr(10,50)+"...");
+//MCCID_INT	Feature_name	Feature_long_description	Feature_short_description	Location_description	Suburb	Feature_type	Event_Activity	Date_from	Date_to	Purpose	Epoch	Language_group	Clan (people)	Nation (group common name)	Source	Source_type	URL	Source_person	Aboriginal_individual	Aboriginal_individual_Gender	Non_Aboriginal_indiv	Non_Aboriginal_indiv_Gender	Non_Aboriginal_organisation    Past and present 	Non_Aboriginal_role	Melway	Aboriginal_words	Aboriginal_words_meaning	European_site_names	Physical_evidence	Address
+  }
+
+  function parse_sculptures(data){
+    console.log("sculptures: ..."+data.substr(10,50)+"...");
+//Description	Title	Co-ordinates
+  }
+
+  function parse_organizations(data){
+    console.log("organizations: ..."+data.substr(10,50)+"...");
   }
 
   function parse(data){
-    console.log("DATA: ..."+data.substr(10,50)+"..."); //50 chars
+    console.log("DATA: ..."+data.substr(10,50)+"...");
+//Organisation	Street	Suburb/Town	Postcode	Website
   }
 
   load("dat/photo_stories.csv");
-//Title	URL	Date	Primary image	Primary image caption	Primary image rights information	Subjects	Station	State	Place	Keywords	Latitude	Longitude	MediaRSS URL
   load("dat/indigenous_heritage.csv");
-//MCCID_INT	Feature_name	Feature_long_description	Feature_short_description	Location_description	Suburb	Feature_type	Event_Activity	Date_from	Date_to	Purpose	Epoch	Language_group	Clan (people)	Nation (group common name)	Source	Source_type	URL	Source_person	Aboriginal_individual	Aboriginal_individual_Gender	Non_Aboriginal_indiv	Non_Aboriginal_indiv_Gender	Non_Aboriginal_organisation    Past and present 	Non_Aboriginal_role	Melway	Aboriginal_words	Aboriginal_words_meaning	European_site_names	Physical_evidence	Address
   load("dat/memorials_and_sculptures.csv");
-//Description	Title	Co-ordinates
   load("dat/indiginous_organizations.csv");
-//Organisation	Street	Suburb/Town	Postcode	Website
-  load("dat/atsic_regions.mid");
-  load("dat/atsic_regions.mif");
-  load("dat/atsic_wards.mid");
-  load("dat/atsic_wards.mif");
+
+//  load("dat/atsic_regions.mid");
+//  load("dat/atsic_regions.mif");
+//  load("dat/atsic_wards.mid");
+//  load("dat/atsic_wards.mif");
 //Ok this ones a bit more complex...
 // .mid contains "names" (hopefully in order?!?)
 //    - one per line, pairs of strings
@@ -113,6 +126,9 @@ window.onload = function() {
   
   ////////////////////////////////////////////////////////////////////////
   // combine data into picture / text streams ////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Block till everythings done... spinner? /////////////////////////////
 
   ////////////////////////////////////////////////////////////////////////
   // Display list ////////////////////////////////////////////////////////
