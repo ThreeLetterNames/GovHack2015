bf8e071bffc59c9ff9825614176fcf4ea5be60b3
PuZZleDucK <puzzleduck@gmail.com>
2015-07-04 12:47:50 +1000

    basic <div> output

--- index.js
+++ index.js
@@ -3,6 +3,23 @@
 window.onload = function() {
   console.log("loading");
   
+  // read in data files
+  
+  // aquire geo location
+  
+  // search data an sort by distance (with areas priooritized first)
+  
+  // combine data into single stream
+  
+  // Display list
+  var output_div = document.getElementById("output");
+  console.log(output_div);
+  output_div.innerHTML += "<h4>Region:</h4>"
+  output_div.innerHTML += "<h5>Regional InformationStats</h5>"
+  output_div.innerHTML += "<h4>Places of Interest:</h4>"
+  output_div.innerHTML += "<h5> - location 1</h5>"
+  output_div.innerHTML += "<h5> - location 2</h5>"
+  
 
 }//onload
 
