69c653ecd48a72e7c2896d61af9206ce57787c1c
PuZZleDucK <puzzleduck@gmail.com>
2015-07-04 16:05:23 +1000

    Added prototypes for reading data, displaying output and acquiring geo location.

--- index.js
+++ index.js
@@ -2,16 +2,65 @@
 
 window.onload = function() {
   console.log("loading");
+
+  ////////////////////////////////////////////////////////////////////////
+  // read in data files //////////////////////////////////////////////////
+function load(file) {
+    var request;
+    if (window.XMLHttpRequest) {
+        // IE7+, Firefox, Chrome, Opera, Safari
+        request = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        request = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    // load
+    request.open('GET', file, false);
+    request.send();
+    parse(request.responseText);
+}
+
+function parse(data){
+    console.log(data);
+}
+
+load("index.html");
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////
+  // aquire geo location /////////////////////////////////////////////////
+  function getLocation() {
+    if (navigator.geolocation) {
+      return navigator.geolocation.getCurrentPosition(geoCallback);
+    }
+    else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function geoCallback(position)
+  {
+    var lonlat="["+position.coords.longitude+","+position.coords.latitude+"]";
+//      alert("GL."+lonlat);
+    geolocation = lonlat;
+    console.log(lonlat);
+    return lonlat;
+  }
+
+  lonlat = getLocation();
+  console.log(lonlat); //not ready untill user interacts
+
+
+  ////////////////////////////////////////////////////////////////////////
+  // search data an sort by distance (with areas priooritized first) /////
   
-  // read in data files
-  
-  // aquire geo location
-  
-  // search data an sort by distance (with areas priooritized first)
-  
-  // combine data into single stream
-  
-  // Display list
+  ////////////////////////////////////////////////////////////////////////
+  // combine data into single stream /////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Display list ////////////////////////////////////////////////////////
   var output_div = document.getElementById("output");
   console.log(output_div);
   output_div.innerHTML += "<h4>Region:</h4>"
